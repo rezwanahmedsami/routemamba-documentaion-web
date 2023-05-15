d0a0c1d0e3683f47272a693ca89467e141708af6
rezwanahmodsami <samiahmed0f0@gmail.com>
2023-05-16 02:48:01 +0600

    Updated

--- documentation.php
+++ documentation.php
@@ -1179,13 +1179,10 @@ console.log("Total count is:", count());
     <div class="container">
       <ul class="social-icons social-icons-lg social-icons-muted
                   justify-content-center mb-3">
-        <li><a data-toggle="tooltip" href="https://twitter.com/harnishdesign/" target="_blank" title="" data-original-title="Twitter"><i class="fab
-                        fa-twitter"></i></a></li>
-        <li><a data-toggle="tooltip" href="http://www.facebook.com/harnishdesign/" target="_blank" title="" data-original-title="Facebook"><i class="fab
+        <li><a data-toggle="tooltip" href="http://www.facebook.com/routemamba/" target="_blank" title="" data-original-title="Facebook"><i class="fab
                         fa-facebook-f"></i></a></li>
-        <li><a data-toggle="tooltip" href="http://www.dribbble.com/harnishdesign/" target="_blank" title="" data-original-title="Dribbble"><i class="fab
-                        fa-dribbble"></i></a></li>
-        <li><a data-toggle="tooltip" href="http://www.github.com/" target="_blank" title="" data-original-title="GitHub"><i class="fab fa-github"></i></a></li>
+
+        <li><a data-toggle="tooltip" href="http://www.github.com/rezwanahmodsami/routemamba" target="_blank" title="" data-original-title="GitHub"><i class="fab fa-github"></i></a></li>
       </ul>
       <p class="text-center">Copyright &copy; 2023 <a href="https://routemamba.com">Routemamba</a>.
         All Rights Reserved.</p>
