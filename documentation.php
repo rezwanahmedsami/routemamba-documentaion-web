dbf9a9d07473ec97f79dd9f002a42860d68540bc
rezwanahmodsami <samiahmed0f0@gmail.com>
2023-08-04 12:46:01 +0600

    updated

--- documentation.php
+++ documentation.php
@@ -209,7 +209,7 @@ Next, let's dive into the installation process and start building powerful singl
               out our video tutorial series:</p>
 
             <div class="video-container">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/WJfOWfGhNNY" title="YouTube
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/xVXzFpfVLlg" title="YouTube
                   video player" frameborder="0" allow="accelerometer; autoplay;
                   clipboard-write; encrypted-media; gyroscope;
                   picture-in-picture; web-share" allowfullscreen></iframe>
